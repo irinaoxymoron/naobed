85a923358710fe00858b7facf2fb2bfbaaa103bb
Irina Andrianova <irinaoxymoron@gmail.com>
2020-09-04 17:51:43 +0300

    slider was added

--- dist/js/js-main.js
+++ dist/js/js-main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    //burger 
     $(".menu-trigger").click(function () {
       $(".ul-trigger").slideToggle();
       $(".menu-trigger").toggleClass('menu_state_open');
@@ -18,4 +19,51 @@ $(document).ready(function(){
         $('.ul-trigger').removeAttr('style');
       };
     });
+
+    // Header fixed scroll effect
+    // var stickyOffset = $('.header').offset().top;
+
+    // $('body').scroll(function(){
+    //   var sticky = $('.header'),
+    //       scroll = $('body').scrollTop();
+        
+    //   if (scroll >= stickyOffset) sticky.addClass('header__fixed');
+    //   else sticky.removeClass('header__fixed');
+    // });
+
+    $(document).ready(function() {
+  
+      var $wrapper = $('.tab-wrapper'),
+          $allTabs = $wrapper.find('.tab-content > div'),
+          $tabMenu = $wrapper.find('.tab-menu li'),
+          $line = $('<div class="line"></div>').appendTo($tabMenu);
+      
+      $allTabs.not(':first-of-type').hide();  
+      $tabMenu.filter(':first-of-type').find(':first').width('100%')
+      
+      $tabMenu.each(function(i) {
+        $(this).attr('data-tab', 'tab'+i);
+      });
+      
+      $allTabs.each(function(i) {
+        $(this).attr('data-tab', 'tab'+i);
+      });
+      
+      $tabMenu.on('click', function() {
+        
+        var dataTab = $(this).data('tab'),
+            $getWrapper = $(this).closest($wrapper);
+        
+        $getWrapper.find($tabMenu).removeClass('active');
+        $(this).addClass('active');
+        
+        $getWrapper.find('.line').width(0);
+        $(this).find($line).animate({'width':'100%'}, 'fast');
+        $getWrapper.find($allTabs).hide();
+        $getWrapper.find($allTabs).filter('[data-tab='+dataTab+']').show();
+      });
+    
+    });//end ready
+
 });
+
