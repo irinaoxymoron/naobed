e183b83ce5976f8bb4e8d568394b77cd76fabe75
Irina Andrianova <irinaoxymoron@gmail.com>
2020-09-07 20:37:49 +0300

    about page was added

--- app/js/js-main.js
+++ app/js/js-main.js
@@ -130,5 +130,16 @@ $(document).ready(function(){
     }
     openLightBox(); //end ready
 
+    // main menu active
+    $(function () { 
+      $('.menu a').each(function () {
+          var location = window.location.href;
+          var link = this.href;
+          if(location == link) {
+              $(this).addClass('active');
+          }
+      });
+    });
+
 });
 
